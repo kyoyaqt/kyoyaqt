873c1e862a7b77960533285fd5bac7c6fedb2d9d
kyoyaqt <riskifed@gmail.com>
2026-06-25 14:33:35 +0800

    faast

--- pemesanan qr/firebase.js
+++ pemesanan qr/firebase.js
@@ -1,12 +1,12 @@
 // Isi config Firebase kamu di sini dari Firebase Console.
 // Jika config belum diisi, aplikasi akan tetap berjalan dengan fallback localStorage.
 window.FIREBASE_CONFIG = window.FIREBASE_CONFIG || {
-    apiKey: "ISI_API_KEY",
-    authDomain: "ISI_AUTH_DOMAIN",
-    projectId: "ISI_PROJECT_ID",
-    storageBucket: "ISI_STORAGE_BUCKET",
-    messagingSenderId: "ISI_MESSAGING_SENDER_ID",
-    appId: "ISI_APP_ID",
+    apiKey: "AIzaSyBRoyT2ogH-9jvhCWEEhd46pa0J98CSUlM",
+    authDomain: "kyoya-pro.firebaseapp.com",
+    projectId: "kyoya-pro",
+    storageBucket: "kyoya-pro.firebasestorage.app",
+    messagingSenderId: "964390632463",
+    appId: "1:964390632463:web:3a350c36c0ec915c8e777a",
 };
 
 window.firebaseApp = null;
